cba788f48c6845d209504a7eb54f5228ec431a16
Ricardo Henrnadez <86048677+Rickhern12345@users.noreply.github.com>
2021-11-16 17:53:18 -0800

    Add files via upload

---
+++ javascript/resumeData.js
@@ -0,0 +1,15 @@
+let resumeData = {
+    "rickyhern12345":"https://rickyhern12345.github.io/",
+    "name": "ricardo hernandez",
+    "role": "computer science major",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"github",
+          "url":"http://github.com/rickyhern12345",
+          
+        },
+    
+    }
+  
+  export default resumeData
